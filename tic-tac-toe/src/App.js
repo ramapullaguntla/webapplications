1ac4e008ff10b6732c8326404f9819aff1f7ef67
Rama Pullaguntla <rama.pullaguntla@gmail.com>
2021-07-28 00:50:08 -0400

    adding squares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component 
+{
+  render()
+  {
+    return (
+      <Board/>      
+    );
+  }
+}
+
+class Square extends React.Component
+{
+  render()
+  {
+    return(
+      <button className="each-square">{this.props.nodevalue}</button>
+    );
+  }
+}
+
+class Board extends React.Component
+{
+  render()
+  {
+    return(
+      <div className="eachrow">
+        <div>
+          <Square nodevalue="1"/>
+          <Square nodevalue="2"/>
+          <Square nodevalue="3"/>    
+        </div>
+        <div>
+          <Square nodevalue="4"/>
+          <Square nodevalue="5"/>
+          <Square nodevalue="6"/>      
+        </div>
+        <div>
+          <Square nodevalue="7"/>
+          <Square nodevalue="8"/>
+          <Square nodevalue="9"/>    
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;

f63f1f6aea35bd22ad29af2f2a239dc25a7f915d
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2021-08-19 10:48:50 -0400

    Add contactlist

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -2,26 +2,48 @@ import './App.css';
 import Header from './components/Header';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList'
+import React, { useEffect, useState } from 'react';
+import {uuid} from 'uuidv4';
 
-const contacts = [
-  {
-      id: "123",
-      "name": "John",
-      "email": "john@gmail.com"
-  },
-  {
-      id: "124",
-      "name": "Mark",
-      "email": "mark@gmail.com"
-  },
-];
 
 function App() {
+ 
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) =>
+    {    
+      console.log("New contact: " + JSON.stringify(contact));
+      setContacts([...contacts, {id: uuid(), ...contact}]);   
+      
+      console.log("All contacts: " + JSON.stringify(contacts));
+    };
+
+  const removeContactHandler = (id) =>
+  {
+    const newContactList = contacts.filter( (c) => c.id !== id);
+    setContacts(newContactList);
+  }
+
+    useEffect(() =>
+    {
+        const retrieveStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if(retrieveStorage)
+        {
+          setContacts(retrieveStorage);
+        }
+    }, []);
+
+    useEffect(() =>
+    {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    }, [contacts]);
+  
   return (
     <div className="ui container">
        <Header/>
-       <AddContact/>
-       <ContactList contacts={contacts}/>
+       <AddContact addContactHandler={addContactHandler}/>
+       <ContactList contacts={contacts} getContactId={removeContactHandler}/>
     </div>
   );
 }
--- contacts/src/components/ContactList.js
+++ contacts/src/components/ContactList.js
@@ -1,30 +1,47 @@
 import React from 'react'
+import ContactCard from './ContactCard';
 
 
-class ContactList extends React.Component
-{   
-    constructor(props)
-    {
-        super(props);
-    }
-    render()
-    {
-        console.log(this.props);
-        return(
-            <div className="ui celled list">{this.renderList}</div>
-        );
-    }
+// class ContactList extends React.Component
+// {      
+//     render()
+//     {        
+//         return(
+//             <div className="ui celled list">{this.renderList}</div>
+//         );
+//     }
 
-    renderList = this.props.contacts.map((contact) =>
-    {
-        return(
-            <div>
-                <div>{contact.name}</div>
-                <div>{contact.email}</div>
-            </div>
-            );
-    });
+//     deleteHandler = (id) =>
+//     {
+//         this.props.getContactId(id);
+//     }
+
+//     renderList = this.props.contacts.map((contact) =>
+//     {
+//         console.log("inside renderlist: " + JSON.stringify(this.props));
+//         return(
+//              <ContactCard contactdetail = {contact} clickHandler={this.deleteHandler} key={contact.id}></ContactCard>
+//             );
+//     });
     
-}
+// }
+
+const ContactList = (props) => {
+    console.log(props);
+  
+    const deleteContactHandler = (id) => {
+      props.getContactId(id);
+    };
+    const renderContactList = props.contacts.map((contact) => {
+      return (
+        <ContactCard
+          contactdetail={contact}
+          clickHandler={deleteContactHandler}
+          key={contact.id}
+        />
+      );
+    });
+    return <div className="ui celled list">{renderContactList}</div>;
+  };
 
 export default ContactList;

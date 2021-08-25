d45b6e899e36dc18127946c587da541355677205
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2021-08-25 15:40:32 -0400

    add JSON server api

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -4,19 +4,29 @@ import AddContact from './components/AddContact';
 import ContactList from './components/ContactList'
 import React, { useEffect, useState } from 'react';
 import {uuid} from 'uuidv4';
-
+import api from './api/contacts-api';
 
 function App() {
  
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (contact) =>
+  const getContacts = async () =>
+  {
+      const response = await api.get("/contacts");
+      return response.data;
+  };
+
+  
+  const addContactHandler = async (contact) =>
     {    
       console.log("New contact: " + JSON.stringify(contact));
-      setContacts([...contacts, {id: uuid(), ...contact}]);   
+      //setContacts([...contacts, {id: uuid(), ...contact}]);   
       
-      console.log("All contacts: " + JSON.stringify(contacts));
+      var uniqueid = uuid();
+      const addResponse = await api.post("/contacts", {id:uniqueid, ...contact});
+      setContacts([...contacts, {id: uniqueid, ...contact}]);
+      console.log("All contacts: " + JSON.stringify(contacts));      
     };
 
   const removeContactHandler = (id) =>
@@ -27,11 +37,18 @@ function App() {
 
     useEffect(() =>
     {
-        const retrieveStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        if(retrieveStorage)
+        //const retrieveStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        const retrieveStorage = async () =>
         {
-          setContacts(retrieveStorage);
-        }
+            const allContacts = await getContacts();
+            if(allContacts)
+              {
+                setContacts(allContacts);
+              }
+        };
+
+        retrieveStorage();
+        
     }, []);
 
     useEffect(() =>

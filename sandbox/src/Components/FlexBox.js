d3c8672d0136bab59720af8938902120c99693e6
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2021-09-10 13:40:46 -0400

    add flexbox

---
+++ sandbox/src/Components/FlexBox.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/Flexbox.css' 
+
+class FlexBox extends React.Component
+{
+    render()
+    {
+        return(
+            <div className="flexcontainer">
+                <div className="box box1">1</div>
+                <div className="box box2">2</div>
+                <div className="box box3">3</div>
+                <div className="box box4">4</div>
+                <div className="box box5">5</div>
+                <div className="box box6">6</div>
+                <div className="box box7">7</div>
+                <div className="box box8">8</div>
+                <div className="box box9">9</div>
+                <div className="box box10">10</div>
+            </div>
+        );
+    }
+}
+
+export default FlexBox;

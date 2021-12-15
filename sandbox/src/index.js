046931e20081b121203f6df0108318261a49bd28
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2021-12-15 10:24:03 -0500

    add sandbox

--- sandbox/src/index.js
+++ sandbox/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux';
 import store from './redux/store'
+import LifecycleTest from './Components/Lifecycle/LifecycleTest';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <Provider store ={store}>
-    <App />
+    <LifecycleTest />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

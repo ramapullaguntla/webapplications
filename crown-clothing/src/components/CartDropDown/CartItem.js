0b1a4b4e4822eb7aec736946ad3b99272725e78c
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2022-10-13 23:56:54 -0400

    adding image to cart dropdown

--- crown-clothing/src/components/CartDropDown/CartItem.js
+++ crown-clothing/src/components/CartDropDown/CartItem.js
@@ -4,7 +4,10 @@ const CartItem = ({item}) =>
 {
    return (
     <div style={{fontSize: '15px'}}>
-        <div>{item.name}</div>
+        <div style={{display:'flex', alignContent:'center'}}>
+            <img style={{height:'20px', width:'20px', marginRight: '5px'}} src={item.imageUrl} alt={item.name}></img>
+            <div>{item.name}</div>
+        </div>
         <div>{item.qty} X {item.price}</div>
     </div>
    );

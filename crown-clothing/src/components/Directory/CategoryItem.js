5fbfe754471872a0611fca806e6d051ee69cc6a1
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2022-10-17 13:25:34 -0400

    adding home routes

--- crown-clothing/src/components/Directory/CategoryItem.js
+++ crown-clothing/src/components/Directory/CategoryItem.js
@@ -1,11 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import '../styles/CategoryItem.css'
 
 const CategoryItem = ({item}) =>
 {
+    const navigate = useNavigate();
+    const navigateHandler = () =>
+    {
+        navigate(item.route);
+    }
     return(
         <div className="category-item" style={{ backgroundImage: `url(${item.imageUrl})`}}>
 
-            <div className='category-item-body'>
+            <div className='category-item-body' onClick={navigateHandler}>
             <h1>{item.title}</h1>
              <p>Shop Now</p>
             </div>
--- crown-clothing/src/components/HomeComponent/Home.js
+++ crown-clothing/src/components/HomeComponent/Home.js
@@ -6,27 +6,32 @@ const Home = () =>
         {
           "id": 1,
           "title": "hats",
-          "imageUrl": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
+          "imageUrl": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+          "route" : "shop/hats"
         },
         {
           "id": 2,
           "title": "jackets",
-          "imageUrl": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"
+          "imageUrl": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+          "route" : "shop/jackets"
         },
         {
           "id": 3,
           "title": "sneakers",
-          "imageUrl": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+          "imageUrl": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+          "route" : "shop/sneakers"
         },
         {
           "id": 4,
           "title": "womens",
-          "imageUrl": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+          "imageUrl": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+          "route" : "shop/womens"
         },
         {
           "id": 5,
           "title": "mens",
-          "imageUrl": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+          "imageUrl": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+          "route" : "shop/mens"
         }
       ];
     

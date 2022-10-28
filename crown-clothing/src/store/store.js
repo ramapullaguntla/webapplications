f51dbe90fc99612344d14092545df1aa91fb8e86
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2022-10-28 17:27:18 -0400

    add redux dev tools

--- crown-clothing/src/store/store.js
+++ crown-clothing/src/store/store.js
@@ -7,5 +7,12 @@ import { rootReducer } from "./rootReducer";
 
 const middlewares = [logger];
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+
 export const store = createStore(rootReducer, undefined, composedEnhancers);

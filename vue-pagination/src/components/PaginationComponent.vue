4d556691a17ca9f5a275f91e87b7d6d5867489bd
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-04-28 21:26:05 -0400

    add vue pagination

---
+++ vue-pagination/src/components/PaginationComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="handleFirstPage" :disabled="currentPage === 1">First</button>
+    <button @click="handlePrevPage" :disabled="currentPage === 1">Previous</button>
+    <span>Page {{ currentPage }}</span>
+    <button @click="handleNextPage" :disabled="currentPage === totalPages">Next</button>
+    <button @click="handleLastPage" :disabled="currentPage === totalPages">Last</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    totalPages: {
+      type: Number,
+      required: true
+    },
+    handleFirstPage: Function,
+    handlePrevPage: Function,
+    handleNextPage: Function,
+    handleLastPage: Function
+  }
+};
+</script>
---
+++ vue-pagination/src/components/ProductList.vue
@@ -0,0 +1,21 @@
+<!-- src/components/ProductList.vue -->
+<template>
+    <div>
+      <h1>Products</h1>
+      <ul>
+        <li v-for="product in displayedProducts" :key="product.id">{{ product.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      displayedProducts: {
+        type: Array,
+        required: true
+      }
+    }
+  };
+  </script>
+  

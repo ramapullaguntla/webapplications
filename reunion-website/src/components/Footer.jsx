8da2a9dd88e60fff21a762261665e54069ee8225
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-07 14:38:14 -0400

    commit all

--- reunion-website/src/components/Footer.jsx
+++ reunion-website/src/components/Footer.jsx
@@ -2,10 +2,12 @@
 const Footer = () =>
 {
     return (
-        <div className='w-full  flex flex-col items-center space-y-2 py-5 bg-blue-300  md:flex-row md:justify-center md:space-x-10 md:space-y-0'>           
-                <div>Email</div>                               
-                <div>&copy; Copyright 2024 Rama Pullaguntla</div>           
+        <div>
+            <div className='w-full flex flex-col items-center space-y-2 py-5 bg-blue-300  md:flex-row md:justify-center md:space-x-10 md:space-y-0'>           
+                    <div className="underline"><a href="mailto:krishna2583@gmail.com">Email</a></div>                               
+                    <div>Rama Pullaguntla</div>           
             </div>
+        </div>
     );
 }
 
--- reunion-website/src/forms/BlogForm.jsx
+++ reunion-website/src/forms/BlogForm.jsx
@@ -32,22 +32,22 @@ const BlogForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-2 m-5 h-[700px]">
+    <form onSubmit={handleSubmit} className="flex flex-col items-center my-5">
 
-      <div className="flex space-x-5">
-            <label htmlFor="Name" className='text-xl font-medium text-center w-44'>Name:</label>
-            <input className='p-1 border w-80 border-gray-600 rounded-md' type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+            <label htmlFor="Name" className='text-lg font-medium md:text-center'>Name:</label>
+            <input className='p-1 border border-gray-600 rounded-md' type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+      
+      
+            <label htmlFor="title" className='text-lg font-medium md:text-center'>Title:</label>
+            <input className='p-1 border border-gray-600 rounded-md' type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+      
+      
+            <label htmlFor="content" className='text-lg font-medium md:text-center'>Content:</label>
+            <textarea className='p-1 border resize-none min-w-80 min-h-60  border-gray-600 rounded-md'  id="content" value={content} onChange={(e) => setContent(e.target.value)} />
       </div>
-      <div className="flex space-x-5">
-            <label htmlFor="title" className='text-xl font-medium text-center w-44'>Title:</label>
-            <input className='p-1 border w-80 border-gray-600 rounded-md' type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </div>
-      <div className="flex space-x-5">
-            <label htmlFor="content" className='text-xl font-medium text-center w-44'>Content:</label>
-            <textarea className='p-1 border w-80 h-80 resize-none  border-gray-600 rounded-md'  id="content" value={content} onChange={(e) => setContent(e.target.value)} />
-       </div>
-       <div className="space-x-2">
-           <button className="bg-slate-400 rounded-md text-black px-5 py-1 my-5 text-center" type="submit">Post</button>               
+       <div>
+           <button className="text-lg text-white my-3 px-4 bg-blue-600 rounded-md" type="submit">Submit</button>               
         </div>  
     </form>
   );

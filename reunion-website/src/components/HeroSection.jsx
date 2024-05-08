1533ed5bd610c9c4fa2c7b724e127f72d9f9d867
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-08 13:26:28 -0400

    change game over page

--- reunion-website/src/components/HeroSection.jsx
+++ reunion-website/src/components/HeroSection.jsx
@@ -6,9 +6,10 @@ const HeroSection = () =>
     return (
         <div className="max-w-3xl mx-auto my-10 flex flex-col items-center space-y-5 text-center p-4">
                 <div><img src={centerphoto} alt="logo" className='rounded-lg'/></div>
-                <div className='flex space-x-6'>
-                    <Link to='/photos'><button className="bg-cyan-500 py-3 px-8 rounded-full">Photo Gallery</button></Link>
-                    <Link to='/gameintro'><button className="bg-cyan-500 py-3 px-8 rounded-full">Play Guessing Game</button></Link>
+                <div className='flex space-x-4'>
+                    <Link to='/photos'><button className="bg-cyan-500 py-2 px-5 rounded-full">Photos</button></Link>
+                    <Link to='/blogs'><button className="bg-cyan-500 py-2 px-5 rounded-full">Testimonials</button></Link>
+                    <Link to='/gameintro'><button className="bg-cyan-500 py-2 px-5 rounded-full">Play Game</button></Link>
                 </div>                       
             </div>
     );

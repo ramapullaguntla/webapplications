0572ae95ee713971ab8c39d98e1ca6400571f7dd
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-08 19:58:22 -0400

    change color palette

--- reunion-website/src/components/Header.jsx
+++ reunion-website/src/components/Header.jsx
@@ -4,7 +4,7 @@ import spslogo from '../assets/logos/SPSLogo.png';
 const Header = () =>
 {
     return (
-        <div className="bg-blue-300 flex justify-between items-center text-center p-2 rounded-b-md">
+        <div className="bg-cyan-500 flex justify-between items-center text-center p-2 rounded-b-md">
             <div><Link to='/'><img src={spslogo} alt="logo" className='rounded-lg w-20 h-20'/></Link></div>            
             <div className="text-2xl hidden md:block">Siddhartha High School Reunion</div>
             <div className="flex justify-between space-x-5 text-lg">

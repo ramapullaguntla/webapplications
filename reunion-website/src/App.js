d75e847ca89cbadff954cb74046942c4f3c0476d
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-12 21:20:24 -0400

    add videos

--- reunion-website/src/App.js
+++ reunion-website/src/App.js
@@ -8,10 +8,12 @@ import BlogForm from "./forms/BlogForm";
 import PhotoGame from "./components/PhotoGame";
 import GameIntro from "./components/GameIntro";
 import GameOver from "./components/GameOver";
+import VideoGallery from "./components/VideoGallery";
 
 function App() {
 
   const [imageArray, setImages] = useState([]);
+  const [videoArray, setVideos] = useState([]);
   useEffect(() =>
   {
     const importAll = (r) => {
@@ -22,9 +24,15 @@ function App() {
 
     const images = importAll(require.context('./assets/photos', false, /\.(png|jpe?g|svg)$/)); 
 
+    const videos = importAll(require.context('./assets/videos', false, /\.(mp4|MOV)$/)); 
+
     const arrayImages = [];
-    Object.keys(images).map((image, index) =>  arrayImages.push(images[image]));           
+    Object.keys(images).map((image, index) =>  arrayImages.push(images[image]));  
     setImages(arrayImages);
+    
+    const arrayVideos = [];
+    Object.keys(videos).map((vdo, index) =>  arrayVideos.push(videos[vdo]));  
+    setVideos(arrayVideos);
   },[]);
 
   return (    
@@ -36,7 +44,8 @@ function App() {
               <Route path="/photos" element= { <PhotoGallery photos={imageArray} /> } />   
               <Route path="/gameintro" element= { <GameIntro /> } />
               <Route path="/playgame" element= { <PhotoGame /> } />     
-              <Route path="/gameover" element= { <GameOver /> } />             
+              <Route path="/gameover" element= { <GameOver /> } />    
+              <Route path="/videos" element= { <VideoGallery videos={videoArray} /> } />            
             </Route>
     </Routes>   
   );
--- reunion-website/src/components/HeroSection.jsx
+++ reunion-website/src/components/HeroSection.jsx
@@ -8,6 +8,7 @@ const HeroSection = () =>
                 <div><img src={centerphoto} alt="logo" className='rounded-lg'/></div>
                 <div className='flex space-x-4'>
                     <Link to='/photos'><button className="bg-cyan-500 py-2 px-2 rounded-lg">Photos</button></Link>
+                    <Link to='/videos'><button className="bg-cyan-500 py-2 px-2 rounded-lg">Videos</button></Link>
                     <Link to='/blogs'><button className="bg-cyan-500 py-2 px-2 rounded-lg">Testimonials</button></Link>
                     <Link to='/gameintro'><button className="bg-cyan-500 py-2 px-2 rounded-lg">Game</button></Link>
                 </div>                       

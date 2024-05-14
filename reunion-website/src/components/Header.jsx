be1816523ec335c23ccb649351e477d6a96ffb6b
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-14 18:04:30 -0400

    add changes to header

--- reunion-website/src/components/Header.jsx
+++ reunion-website/src/components/Header.jsx
@@ -4,15 +4,21 @@ import spslogo from '../assets/logos/SPSLogo.png';
 const Header = () =>
 {
     return (
-        <div className="bg-cyan-500 flex justify-between items-center text-center p-2 rounded-b-md">
-            <div><Link to='/'><img src={spslogo} alt="logo" className='rounded-lg w-20 h-20'/></Link></div>            
-            <div className="text-2xl hidden md:block">Siddhartha High School Reunion</div>
-            <div className="flex justify-between space-x-3 text-lg mx-1">
-            <Link to='/'><div>Home</div></Link>
-            <Link to='/photos'><div>Photos</div></Link>
-            <Link to='/videos'><div>Videos</div></Link>
-            <Link to='/blogs'><div>Testimonials</div></Link>   
-            <Link to='/gameintro'><div>Game</div></Link>           
+        <div>
+            <div className="xs:hidden sm:hidden  bg-cyan-500 md:flex justify-between items-center text-center p-2 rounded-b-md">
+                <div><Link to='/'><img src={spslogo} alt="logo" className='rounded-lg w-20 h-20'/></Link></div>            
+                <div className="text-2xl hidden md:block">Siddhartha High School Reunion</div>
+                <div className="flex justify-between space-x-3 text-lg mx-1">
+                <Link to='/'><div>Home</div></Link>
+                <Link to='/photos'><div>Photos</div></Link>
+                <Link to='/videos'><div>Videos</div></Link>
+                <Link to='/blogs'><div>Testimonials</div></Link>   
+                <Link to='/gameintro'><div>Game</div></Link>           
+                </div>
+            </div>
+
+            <div className="xs:block sm:block md:hidden  bg-cyan-500  w-20 mx-auto my-2 text-center rounded-lg">                
+                <Link to='/'><div>Home</div></Link>                                        
             </div>
         </div>
     );

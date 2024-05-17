c1c6ee0a87678a37e341384cd229945f287cc981
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-17 12:51:00 -0400

    changes to videos

--- reunion-website/src/components/VideoGallery.jsx
+++ reunion-website/src/components/VideoGallery.jsx
@@ -2,17 +2,19 @@ const VideoGallery = (props) =>
 {
     const renderImages = () =>
     {     
+        
 
         return (
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 ">              
                 {props.videos.map((vdo, index) => (
-                    <div key={index} className="border border-gray-400">
-                        <video controls>
-                            <source src={vdo} type="video/mp4" />
-                            Your browser does not support the video tag.
+                    <div key={index} >
+                        <video controls src={vdo} className="border-2 border-gray-400 rounded-t-md">                           
                         </video>
+                        <div className="bg-slate-200 rounded-b-md font-semibold p-2">
+                            Performance by Praneeth, bhavani and shyam
+                        </div>
                     </div>
-                  ))}
+                  ))}                
             </div>
            
           );        

17467115b133c2e3af58268e03c2b924f8a54d25
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-17 15:56:15 -0400

    add more videos

--- reunion-website/src/videoInfo.js
+++ reunion-website/src/videoInfo.js
@@ -14,21 +14,26 @@ export const videoInfo = [
     {
       "id": 3,
       "title": "Ranjithame",
-      "description" : "Rocking Solo performance by Shyam",
+      "description" : "Rocking Solo performance by Shyam for this mass number.",
       "videoName": "Ranjithame.mp4",
       
     },
     {
       "id": 4,
-      "title": "Musthafa",
-      "description" : "This time everyone participated in this memorable song of friendship.",
-      "videoName": "Musthafa.mp4",
-    },  
-
+      "title": "Oye Oye",
+      "description" : "Praneeth shows his impressive dance skills in this peppy solo.",
+      "videoName": "Oye Oye.mp4",
+    },    
     {
       "id": 5,
       "title": "Guvva Gorinka",
       "description" : "Bhavani and Praneeth come together to recreate this timeless masterpiece.",
       "videoName": "Guvva Gorinka.mp4",
-    },    
+    }, 
+    {
+      "id": 6,
+      "title": "Musthafa",
+      "description" : "This time everyone participated in this memorable song of friendship.",
+      "videoName": "Musthafa.mp4",
+    } 
   ];

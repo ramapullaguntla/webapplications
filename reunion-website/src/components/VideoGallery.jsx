94d98f3591ea58e27ee337c4aeb659dc2d91937b
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-05-17 14:51:09 -0400

    add video info

--- reunion-website/src/components/VideoGallery.jsx
+++ reunion-website/src/components/VideoGallery.jsx
@@ -1,17 +1,20 @@
+import { videoInfo } from "../videoInfo";
+
 const VideoGallery = (props) =>
-{
+{    
     const renderImages = () =>
     {     
         
 
         return (
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 ">              
-                {props.videos.map((vdo, index) => (
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2 ">              
+                {videoInfo.map((vdo, index) => (
                     <div key={index} >
-                        <video controls src={vdo} className="border-2 border-gray-400 rounded-t-md">                           
+                        <video controls src={require('../assets/videos/' + vdo.videoName)} className="border-2 border-gray-400 rounded-t-md">                           
                         </video>
-                        <div className="bg-slate-200 rounded-b-md font-semibold p-2">
-                            Performance by Praneeth, bhavani and shyam
+                        <div className="bg-slate-300 rounded-b-md p-2 min-h-24">
+                            <div className="text-lg font-bold">{vdo.title}</div>
+                            <div className="text-base">{vdo.description}</div>
                         </div>
                     </div>
                   ))}                
--- reunion-website/src/videoInfo.js
+++ reunion-website/src/videoInfo.js
@@ -1,159 +1,34 @@
 export const videoInfo = [
     {
       "id": 1,
-      "options": ["bhavani", "sudha", "hyndavi", "radhika"],
-      "imageUrl": "bhavani.png",
-      "answer" : "bhavani"
+      "title": "Kaala chashma",
+      "description" : "Amazing Performance by Praneeth, Bhavani, Rajasekhar and Shyam on this groovy song",
+      "videoName": "Kala Chashma.mp4"      
     },
     {
       "id": 2,
-      "options": ["bhavani", "swathi", "saritha", "radhika"],
-      "imageUrl": "radhika.png",
-      "answer" : "radhika"
+      "title": "Kaala chashma Encore",
+      "description" : "Crowd demands an encore, this time with the goggles (kaala chashma).",
+      "videoName": "Kala Chashma Encore.mp4"
     },
     {
       "id": 3,
-      "options": ["Neelima", "sharmila", "hyndavi", "vineela"],
-      "imageUrl": "Neelima.png",
-      "answer" : "Neelima"
+      "title": "Ranjithame",
+      "description" : "Rocking Solo performance by Shyam",
+      "videoName": "Ranjithame.mp4",
+      
     },
     {
       "id": 4,
-      "options": ["vamshi", "saikiran", "ravi kiran", "laxmikanth"],
-      "imageUrl": "saikiran.png",
-      "answer" : "saikiran"
-    },
+      "title": "Musthafa",
+      "description" : "This time everyone participated in this memorable song of friendship.",
+      "videoName": "Musthafa.mp4",
+    },  
+
     {
       "id": 5,
-      "options": ["rama", "sai kiran", "soma sundar", "chandra"],
-      "imageUrl": "chandra.png",      
-      "answer" : "chandra"
-    },
-
-    {
-      "id": 6,
-      "options": ["swathi", "anuradha", "saritha", "neelima"],
-      "imageUrl": "saritha.png",
-      "answer" : "saritha"
-    },
-    {
-      "id": 7,
-      "options": ["radhika", "swathi", "hemabhavani", "radha"],
-      "imageUrl": "hemabhavani.png",
-      "answer" : "hemabhavani"
-    },
-    {
-      "id": 8,
-      "options": ["praneeth", "ganesh", "rama", "raghu"],
-      "imageUrl": "ganesh.png",
-      "answer" : "ganesh"
-    },
-    {
-      "id": 9,
-      "options": ["vineela", "radhika", "suneetha", "hyndavi"],
-      "imageUrl": "hyndavi.png",
-      "answer" : "hyndavi"
-    },
-    {
-      "id": 10,
-      "options": ["satish", "laxmikanth", "soma sundar", "ravichander"],
-      "imageUrl": "laxmikanth.png",      
-      "answer" : "laxmikanth"
-    },
-
-    {
-      "id": 11,
-      "options": ["Murali", "praneeth", "shyam", "rajasekhar"],
-      "imageUrl": "Murali.png",
-      "answer" : "Murali"
-    },
-    {
-      "id": 12,
-      "options": ["rama", "vamshi", "ganesh", "ravikiran"],
-      "imageUrl": "rama.png",
-      "answer" : "rama"
-    },
-    {
-      "id": 13,
-      "options": ["shyam", "Ravichander", "rama", "satish"],
-      "imageUrl": "Ravichander.png",
-      "answer" : "Ravichander"
-    },
-    {
-      "id": 14,
-      "options": ["laxmikanth", "chandra", "ravikiran", "saikiran"],
-      "imageUrl": "ravikiran.png",
-      "answer" : "ravikiran"
-    },
-    {
-      "id": 15,
-      "options": ["satish", "laxmikanth", "pradeep", "murali"],
-      "imageUrl": "satish.png",      
-      "answer" : "satish"
-    },
-
-    {
-      "id": 16,
-      "options": ["bhavani", "sharmila", "vineela", "sudha"],
-      "imageUrl": "sharmila.png",
-      "answer" : "sharmila"
-    },
-    {
-      "id": 17,
-      "options": ["chandra", "vamshi", "ravichander", "shyam"],
-      "imageUrl": "shyam.png",
-      "answer" : "shyam"
-    },
-    {
-      "id": 18,
-      "options": ["rajasekhar", "Ravichander", "somasundar", "murali"],
-      "imageUrl": "somasundar.png",
-      "answer" : "somasundar"
-    },
-    {
-      "id": 19,
-      "options": ["rama", "Vamshi", "ganesh", "ravikiran"],
-      "imageUrl": "Vamshi.png",
-      "answer" : "Vamshi"
-    },
-    {
-      "id": 20,
-      "options": ["hemabhavani", "sudha", "sharmila", "hyndavi"],
-      "imageUrl": "sudha.png",      
-      "answer" : "sudha"
-    },
-
-    {
-      "id": 21,
-      "options": ["swathi", "bhavani", "radhika", "neelima"],
-      "imageUrl": "swathi.png",
-      "answer" : "swathi"
-    },
-    {
-      "id": 22,
-      "options": ["radhika", "Vineela", "sharmila", "hemabhavani"],
-      "imageUrl": "Vineela.png",      
-      "answer" : "Vineela"
-    },
-
-    {
-      "id": 23,
-      "options": ["saritha", "hyndavi", "radhika", "neelima"],
-      "imageUrl": "radhika1.png",      
-      "answer" : "radhika"
-    },
-
-    {
-      "id": 24,
-      "options": ["rama", "shyam", "praneeth", "ravichander"],
-      "imageUrl": "praneeth.png",
-      "answer" : "praneeth"
-    },
-    {
-      "id": 25,
-      "options": ["murali", "satish", "ganesh", "rajasekhar"],
-      "imageUrl": "rajasekhar.png",      
-      "answer" : "rajasekhar"
-    }
-
+      "title": "Guvva Gorinka",
+      "description" : "Bhavani and Praneeth come together to recreate this timeless masterpiece.",
+      "videoName": "GuvvaGorinka.mp4",
+    },    
   ];

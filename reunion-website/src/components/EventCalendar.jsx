b397d0c6e5332ee3db296f02e35aec0aeb6eb739
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-09-05 18:55:49 -0400

    add saturday and sunday

--- reunion-website/src/components/EventCalendar.jsx
+++ reunion-website/src/components/EventCalendar.jsx
@@ -22,7 +22,7 @@ const EventCalendar = () => {
     isLoading ? <div className='flex items-center space-x-2'><div>Loading...</div><div className='bg-green-700 w-3 h-3 rounded-full animate-bounce'></div></div> 
     : <div className='bg-slate-50 my-3 flex flex-col items-center'>
       <h2 className='text-xl font-bold text-center my-3'>Event Calendar for the Ganesh Chaturdi Celebration.</h2>
-      <div>
+      <div className='flex-col items-center space-y-4'>
             <div className="">
                         <h2 className='text-xl font-bold mb-1 text-center'>Friday</h2>
                         <table className='tab'>
@@ -44,7 +44,54 @@ const EventCalendar = () => {
                             </tbody>
                         </table>
             </div>
-      </div>             
+            <div className="">
+                        <h2 className='text-xl font-bold mb-1 text-center'>Saturday</h2>
+                        <table className='tab'>
+                            <thead>
+                            <tr className="bg-gray-200">
+                                <th className="border border-gray-300 px-4 py-2">Name</th>
+                                <th className="border border-gray-300 px-4 py-2">Start</th>
+                                <th className="border border-gray-300 px-4 py-2">End</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {events.filter(e => e.day === "Saturday").sort((a,b) => a.order - b.order).map((event, index) => (
+                                <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
+                                <td className="border border-gray-300 px-4 py-1">{event.name}</td>
+                                <td className="border border-gray-300 px-4 py-1">{event.startTime}</td>
+                                <td className="border border-gray-300 px-4 py-1">{event.endTime}</td>
+                            </tr>
+                            ))}
+                            </tbody>
+                        </table>
+            </div>
+            <div className="">
+                        <h2 className='text-xl font-bold mb-1 text-center'>Sunday</h2>
+                        <table className='tab'>
+                            <thead>
+                            <tr className="bg-gray-200">
+                                <th className="border border-gray-300 px-4 py-2">Name</th>
+                                <th className="border border-gray-300 px-4 py-2">Start</th>
+                                <th className="border border-gray-300 px-4 py-2">End</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {events.filter(e => e.day === "Sunday").sort((a,b) => a.order - b.order).map((event, index) => (
+                                <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
+                                <td className="border border-gray-300 px-4 py-1">{event.name}</td>
+                                <td className="border border-gray-300 px-4 py-1">{event.startTime}</td>
+                                <td className="border border-gray-300 px-4 py-1">{event.endTime}</td>
+                            </tr>
+                            ))}
+                            </tbody>
+                        </table>
+            </div>
+      </div>  
+       
+
+      <div>
+            
+      </div>         
       
     </div>
   );

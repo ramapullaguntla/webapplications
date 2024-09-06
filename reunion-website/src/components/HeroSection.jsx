ba3221fd0f8ad1f9054ae714794a2bdb8fb960f4
Rama Pullaguntla <Rama.Pullaguntla@hyland.com>
2024-09-06 16:04:54 -0400

    add flyer

--- reunion-website/src/components/HeroSection.jsx
+++ reunion-website/src/components/HeroSection.jsx
@@ -1,12 +1,15 @@
-import centerphoto from '../assets/olmphotos/GaneshChaturdi2024.jpg';
+import eventsflyer from '../assets/olmphotos/EventsFlyer.jpg';
 import flyer from '../assets/olmphotos/GaneshFlyer.jpg';
 import { Link } from 'react-router-dom';
 
 const HeroSection = () =>
 {
     return (
-        <div className="max-w-3xl mx-auto my-10 flex flex-col items-center space-y-5 text-center p-4">
-                <div><img src={flyer} alt="logo" className='rounded-lg'/></div>
+        <div className="max-w-4xl mx-auto my-10 flex flex-col items-center space-y-5 text-center p-2">
+                <div className='flex flex-wrap justify-around  p-1 w-full'>
+                    <img src={flyer} alt="logo" className='rounded-lg w-[370px] h-[490px] mb-1'/>
+                    <img src={eventsflyer} alt="logo" className='rounded-lg w-[370px] h-[490px]'/>
+                 </div>
                 <div className='flex space-x-4'>
                     <Link to='/photos'><button className="bg-header-brown py-2 px-2 rounded-lg text-gray-50">Photos</button></Link> 
                     <Link to='/events'><button className="bg-header-brown py-2 px-2 rounded-lg text-gray-50">Events</button></Link> 

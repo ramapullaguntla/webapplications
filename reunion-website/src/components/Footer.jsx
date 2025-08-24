3509efe4c63a8c061c4b1197263c10c69c269abc
Rama Pullaguntla <rama.pullaguntla@hyland.com>
2025-08-24 17:04:02 -0400

    change footer

--- reunion-website/src/components/Footer.jsx
+++ reunion-website/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () =>
     return (
         <div>
             <div className='text-gray-50 w-full flex flex-col items-center space-y-2 py-5 bg-header-brown  md:flex-row md:justify-center md:space-x-10 md:space-y-0'>           
-                    <div className="underline"><a href="mailto:krishna2583@gmail.com">Email</a></div>                               
+                    <div className="underline"><a href="mailto:krishna2583@gmail.com; venkataravitejachennu@gmail.com">Email</a></div>                               
                     <div>Oaklawn Mills Residents</div>           
             </div>
         </div>
